26e078d1c7fde0f90cd7aa9fef030e4a90887176
Krzysztof Górski <krzysztofmichalgorski@gmail.com>
2025-11-10 01:00:14 +0100

    s

--- random_forest.py
+++ random_forest.py
@@ -5,21 +5,45 @@ import pandas as pd
 
 df = pd.read_csv('deweloperuch_all.csv')
 
-# zakładamy df z kolumnami: Area_m2, Price_total, Price_m2, Rooms, Floor, source, Address
+
 X = df[["Area_m2", "Price_total", "Price_m2", "Rooms", "Floor", "source"]].copy()
 y = df["Address"]
 
-# zakoduj source
+#
 X = pd.get_dummies(X, columns=["source"], drop_first=True)
 
-# podział danych
+# split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# model Random Forest
+# create and train the Random Forest model
 rf_model = RandomForestClassifier(n_estimators=125, random_state=42)
 rf_model.fit(X_train, y_train)
 
-# predykcja i accuracy
+# accuracy
 y_pred = rf_model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Random Forest Accuracy:", accuracy)
+
+# test example
+sample = pd.DataFrame([{
+    "Area_m2": 80,
+    "Price_total": 2400000,
+    "Price_m2": 30000,
+    "Rooms": 5,
+    "Floor": 3,
+    "source_transactions": 0
+}])
+
+for col in X_train.columns:
+    if col not in sample.columns:
+        sample[col] = 0
+sample = sample[X_train.columns]
+
+# prediction of address
+predicted_address = rf_model.predict(sample)
+print("Most likely street:", predicted_address[0])
+
+# weights of features in model
+importances = rf_model.feature_importances_
+for col, imp in zip(X_train.columns, importances):
+    print(f"{col}: {imp:.4f}")
